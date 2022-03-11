5d201f20cb4f0ced1630256d525d8ec4e5321158
armandoj2 <armando.jimenez2@upr.edu>
2022-03-11 19:18:06 -0400

    Added Mod as structure and added first mod to database.

--- mysite/mods/models.py
+++ mysite/mods/models.py
@@ -2,3 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
+class Mod(models.Model):
+    mod_title = models.CharField(max_length=200)
+    mod_author = models.CharField(max_length=200)
+    mod_source = models.CharField(max_length=200)
+    mod_game = models.CharField(max_length=200)
+    mod_description = models.TextField()
+
+    def __str__(self):
+        return self.mod_title
+
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mods.apps.ModsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
