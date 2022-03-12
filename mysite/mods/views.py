d48c5c00213d22ea69bb1d3faa564860548deb44
Gustavo A. Martinez Perez <54903695+SurrealGus@users.noreply.github.com>
2022-03-11 22:15:19 -0400

    Added html files, consisting of basic webpage functions.

--- mysite/mods/views.py
+++ mysite/mods/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .models import Mod
+from django.template import loader
+from django.http import Http404, HttpResponse
 from django.http import HttpResponse
 # Create your views here.
+# from mysite.mods.models import Mod
 
 
 def index(request):
-    return HttpResponse('Hello, this is the mods index')
+    mod_list = Mod.objects.order_by('mod_title')[:5]
+    template = loader.get_template('mods/index.html')
+    context = {
+        'mod_list': mod_list,
+    }
+    return HttpResponse(template.render(context, request))
