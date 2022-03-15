809d139290c100a58a8a202b2140d5ca2b420b59
armandoj2 <armando.jimenez2@upr.edu>
2022-03-15 10:48:58 -0400

    Added basic function views and html templates

--- mysite/mods/urls.py
+++ mysite/mods/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name ='index'),
-    path('LoginForm.html', views.LoginForm, name ='LoginForm')
+    path('LoginForm.html', views.LoginForm, name ='LoginForm'),
+    path('dashboard.html', views.dashboard, name ='dashboard'),
+    path('register.html', views.register, name ='register')
 ]
--- mysite/mods/views.py
+++ mysite/mods/views.py
@@ -3,6 +3,10 @@ from .models import Mod
 from django.template import loader
 from django.http import Http404, HttpResponse
 from django.http import HttpResponse
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+from django.urls import reverse
 # Create your views here.
 # from mysite.mods.models import Mod
 
@@ -15,7 +19,26 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
+
 def LoginForm(request):
     mod_list = Mod.objects.order_by('mod_title')[:5]
     template = loader.get_template('mods/LoginForm.html')
     return HttpResponse(template.render(None, request))
+
+
+def dashboard(request):
+    return render(request, 'mods/dashboard.html')
+
+
+def register(request):
+    if request.method == "GET":
+        return render(
+            request, "mods/register.html",
+            {"form": CustomUserCreationForm}
+        )
+    elif request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse("dashboard"))
