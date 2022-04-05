0a0a48d2dfeaf656f56ac373b00b3867ab3e7cb2
armandoj2 <armando.jimenez2@upr.edu>
2022-04-04 23:28:12 -0400

    40 unit tests added

--- mysite/mods/tests.py
+++ mysite/mods/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
+import random, string
 
-from .models import Mod
+from .models import Mod, Discussion, Reply
 from .forms import CustomUserCreationForm, PublishForm, ReplyForm, DiscussionForm
 
 
@@ -13,6 +14,16 @@ class ModTestClass(TestCase):
                            mod_game='test game',
                            mod_source='test source',
                            mod_description='test desc.')
+        number_of_mods = 10
+
+        for mod_id in range(number_of_mods):
+            Mod.objects.create(
+                mod_title= f'title {mod_id}',
+                mod_author= f'author {mod_id}',
+                mod_game=f'game {mod_id}',
+                mod_source=f'source {mod_id}',
+                mod_description=f'description {mod_id}'
+            )
         pass
 
     def test_mod_title(self):
@@ -38,6 +49,9 @@ class ModTestClass(TestCase):
     def test_mod_str(self):
        mod = Mod.objects.get(id=1)
        self.assertEqual(mod.__str__(), 'test title')
+
+    def test_mod_object_creation_count(self):
+        self.assertEqual(11,Mod.objects.count())
 
 
 class PublishFormTestClass(TestCase):
@@ -67,6 +81,292 @@ class PublishFormTestClass(TestCase):
         fields = ['Mod title', 'Mod author', 'Mod game', 'Mod source', 'Mod description']
         self.assertEqual(form.fields['mod_description'].label, fields[4])
 
+    def test_publish_form_valid(self):
+        mod = Mod.objects.create(mod_title='test title',
+                                 mod_author='test author',
+                                 mod_game='test game',
+                                 mod_source='test source',
+                                 mod_description='test desc.')
+        data = {'mod_title': mod.mod_title,
+                'mod_author': mod.mod_author,
+                'mod_game': mod.mod_game,
+                'mod_source': mod.mod_source,
+                'mod_description': mod.mod_description}
+        form = PublishForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_publish_form_invalid_no_title(self):
+        mod = Mod.objects.create(mod_title='',
+                                 mod_author='test author',
+                                 mod_game='test game',
+                                 mod_source='test source',
+                                 mod_description='test description')
+        data = {'mod_title': mod.mod_title,
+                'mod_author': mod.mod_author,
+                'mod_game': mod.mod_game,
+                'mod_source': mod.mod_source,
+                'mod_description': mod.mod_description}
+        form = PublishForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_publish_form_invalid_no_author(self):
+        mod = Mod.objects.create(mod_title='test title',
+                                 mod_author='',
+                                 mod_game='test game',
+                                 mod_source='test source',
+                                 mod_description='test description')
+        data = {'mod_title': mod.mod_title,
+                'mod_author': mod.mod_author,
+                'mod_game': mod.mod_game,
+                'mod_source': mod.mod_source,
+                'mod_description': mod.mod_description}
+        form = PublishForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_publish_form_invalid_no_game(self):
+        mod = Mod.objects.create(mod_title='test title',
+                                 mod_author='test author',
+                                 mod_game='',
+                                 mod_source='test source',
+                                 mod_description='test description')
+        data = {'mod_title': mod.mod_title,
+                'mod_author': mod.mod_author,
+                'mod_game': mod.mod_game,
+                'mod_source': mod.mod_source,
+                'mod_description': mod.mod_description}
+        form = PublishForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_publish_form_invalid_no_source(self):
+        mod = Mod.objects.create(mod_title='test title',
+                                 mod_author='test author',
+                                 mod_game='test game',
+                                 mod_source='',
+                                 mod_description='test description')
+        data = {'mod_title': mod.mod_title,
+                'mod_author': mod.mod_author,
+                'mod_game': mod.mod_game,
+                'mod_source': mod.mod_source,
+                'mod_description': mod.mod_description}
+        form = PublishForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_publish_form_invalid_no_description(self):
+        mod = Mod.objects.create(mod_title='test title',
+                                 mod_author='test author',
+                                 mod_game='test game',
+                                 mod_source='test source',
+                                 mod_description='')
+        data = {'mod_title': mod.mod_title,
+                'mod_author': mod.mod_author,
+                'mod_game': mod.mod_game,
+                'mod_source': mod.mod_source,
+                'mod_description': mod.mod_description}
+        form = PublishForm(data=data)
+        self.assertFalse(form.is_valid())
+
+
+class CustomUserCreationFormTestClass(TestCase):
+
+    def test_user_form_is_valid(self):
+        data = {'username': 'username',
+                'email': 'email@example.com',
+                'password1': 'password',
+                'password2': 'password'}
+        form = CustomUserCreationForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_user_form_is_invalid_diff_passwords(self):
+        data = {'username': 'username',
+                'email': 'email@example.com',
+                'password1': 'password2',
+                'password2': 'password'}
+        form = CustomUserCreationForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_user_form_is_invalid_email(self):
+        data = {'username': 'username',
+                'email': 'email',
+                'password1': 'password',
+                'password2': 'password'}
+        form = CustomUserCreationForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_user_form_is_invalid_no_username(self):
+        data = {'username': '',
+                'email': 'email@example.com',
+                'password1': 'password',
+                'password2': 'password'}
+        form = CustomUserCreationForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_user_form_is_invalid_no_password(self):
+        data = {'username': 'username',
+                'email': 'email@example.com',
+                'password1': '',
+                'password2': ''}
+        form = CustomUserCreationForm(data=data)
+        self.assertFalse(form.is_valid())
+
+
+class DiscussionFormTestClass(TestCase):
+    def test_dis_form_valid(self):
+        data = {'dis_title': 'title',
+                'dis_author': 'author',
+                'dis_type' : 'Mod',
+                'dis': 'Discussion'
+        }
+        form = DiscussionForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_dis_form_invalid_no_title(self):
+        data = {'dis_title': '',
+                'dis_author': 'author',
+                'dis_type': 'Mod',
+                'dis': 'Discussion'
+                }
+        form = DiscussionForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_dis_form_invalid_no_author(self):
+        data = {'dis_title': 'title',
+                'dis_author': '',
+                'dis_type': 'Mod',
+                'dis': 'Discussion'
+                }
+        form = DiscussionForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_dis_form_invalid_no_type(self):
+        data = {'dis_title': 'title',
+                'dis_author': 'author',
+                'dis_type': '',
+                'dis': 'Discussion'
+                }
+        form = DiscussionForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_dis_form_invalid_no_dis(self):
+        data = {'dis_title': 'title',
+                'dis_author': 'author',
+                'dis_type': 'Mod',
+                'dis': ''
+                }
+        form = DiscussionForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_dis_form_invalid_wrong_type(self):
+        data = {'dis_title': 'title',
+                'dis_author': 'author',
+                'dis_type': 'A type',
+                'dis': 'Discussion'
+                }
+        form = DiscussionForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_dis_form_max_char_title(self):
+        letters = string.ascii_letters
+        astring = ''
+        astring = astring.join(random.choice(letters) for i in range(201))
+        data = {'dis_title': astring,
+                'dis_author': 'author',
+                'dis_type': 'Mod',
+                'dis': 'Discussion'
+                }
+        form = DiscussionForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_dis_form_max_char_author(self):
+        letters = string.ascii_letters
+        astring = ''
+        astring = astring.join(random.choice(letters) for i in range(201))
+        data = {'dis_title': 'title',
+                'dis_author': astring,
+                'dis_type': 'Mod',
+                'dis': 'Discussion'
+                }
+        form = DiscussionForm(data=data)
+        self.assertFalse(form.is_valid())
+
+
+class ReplyFormTestClass(TestCase):
+    def setUp(self):
+        Discussion.objects.create(dis_title= 'title',
+                dis_author= 'author',
+                dis_type= 'Mod',
+                dis= 'Discussion'
+        )
+        pass
+
+    def test_rep_form_valid_parent(self):
+        data = {'rep_title': 'title',
+                'rep_author': 'author',
+                'rep_parent': Discussion.objects.get(id=1),
+                'rep': 'rep'
+                }
+        form = ReplyForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_rep_form_valid_no_parent(self):
+        data = {'rep_title': 'title',
+                'rep_author': 'author',
+                'rep_parent': '',
+                'rep': 'rep'
+                }
+        form = ReplyForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_dis_form_invalid_wrong_parent(self):
+        data = {'rep_title': 'title',
+                'rep_author': 'author',
+                'rep_parent': 'Mod',
+                'rep': 'rep'
+                }
+        form = ReplyForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_rep_form_invalid_no_title(self):
+        data = {'rep_title': '',
+                'rep_author': 'author',
+                'rep_parent': '',
+                'rep': 'rep'
+                }
+        form = ReplyForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_rep_form_invalid_no_rep(self):
+        data = {'rep_title': 'title',
+                'rep_author': 'author',
+                'rep_parent': '',
+                'rep': ''
+                }
+        form = DiscussionForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_rep_form_max_char_title(self):
+        letters = string.ascii_letters
+        astring = ''
+        astring = astring.join(random.choice(letters) for i in range(201))
+        data = {'rep_title': astring,
+                'rep_author': 'author',
+                'rep_parent': '',
+                'rep': 'rep'
+                }
+        form = DiscussionForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_rep_form_max_char_author(self):
+        letters = string.ascii_letters
+        astring = ''
+        astring = astring.join(random.choice(letters) for i in range(201))
+        data = {'rep_title': 'title',
+                'rep_author': astring,
+                'rep_parent': '',
+                'rep': 'rep'
+                }
+        form = DiscussionForm(data=data)
+        self.assertFalse(form.is_valid())
+
 
 class NewIndexViewTestClass(TestCase):
     # def setUp(self):
@@ -94,3 +394,4 @@ class NewIndexViewTestClass(TestCase):
         response = self.client.get(reverse('NewIndex'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'mods/NewIndex.html')
+
